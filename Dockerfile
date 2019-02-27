3282bf11ce62b092917e170e1eef04b634a68ffd
Masahiro Yamauchi <sgt.yamauchi@gmail.com>
2019-02-27 11:16:35 +0900

    Main

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM python:3.7.2-alpine
+
+RUN apk add --no-cache --update alpine-sdk
+RUN pip install mmh3
+COPY main.py ./
+CMD ["main.py"]
+ENTRYPOINT ["python"]
---
+++ main.py
@@ -0,0 +1,19 @@
+import sys
+import mmh3
+
+def get_hash(data, seed=None):
+    if seed is None:
+        return mmh3.hash(data)
+    return mmh3.hash(data, seed)
+
+def main(args):
+    if len(args) > 1:
+        return get_hash(args[0], int(args[1]))
+    return get_hash(args[0])
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) == 0:
+        print('Usage: docker run algas/murmur-cli <target_string> [seed (option)]')
+    print(main(args[1:]))
